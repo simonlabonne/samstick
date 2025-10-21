3ad942ec6b53049405bb5ee4ecaf141ba2e0509d
Simon Labonne <simon.labonne@mcgill.ca>
2025-10-20 21:48:16 -0400

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,29 @@ export default function Home() {
                     <Image src="/logo.svg" alt="Logo Samstick" width={200} height={200} />
                 </div>
                 <h1 className="text-4xl font-bold mb-4">Samstick</h1>
-                <p className="text-lg mb-4">Testez vos connaissances sur le hockey en devinant si le joueur est droitier ou gaucher, ou s'il a du tape noir ou blanc.
-                    <br />Jeu inspiré par <a href="https://www.instagram.com/montembeault.samuel/">@montembeault.samuel</a> et <a href="https://www.instagram.com/drettesultape/">@drettesultape</a>.</p>
+                <p className="text-lg mb-4">
+                    Testez vos connaissances sur le hockey en devinant si le joueur est droitier ou gaucher, ou s'il a du tape noir ou blanc.
+                    <br />
+                    Jeu inspiré par <a href="https://www.instagram.com/montembeault.samuel/">&apos;@montembeault.samuel&apos;</a> et <a href="https://www.instagram.com/drettesultape/">&apos;@drettesultape&apos;</a>.
+                </p>
             </div>
             <div className="container px-4 lg:px-0 lg:mx-auto text-center mt-8">
                 <h2 className="text-3xl font-bold mb-4">Gaucher ou droitier</h2>
                 <ul className="grid grid-cols-1 gap-4">
                     {Array.from({ length: 6 }).map((_, i) => (
                         <li key={i} className="">
-                            <Link href={`/niveau-${i + 1}`} className="bg-white hover:bg-gray-100 p-4 rounded-lg shadow border border-gray-100 mx-auto inline-block text-center text-lg font-bold">
-                                Niveau {i + 1}
+                            <Link href={`/niveau-${i + 1}`}>
+                                <a className="bg-white hover:bg-gray-100 p-4 rounded-lg shadow border border-gray-100 mx-auto inline-block text-center text-lg font-bold">
+                                    Niveau {i + 1}
+                                </a>
                             </Link>
                         </li>
                     ))}
                     <li className="">
-                        <Link href={`/niveau-2005-2015`} className="bg-white hover:bg-gray-100 p-4 rounded-lg shadow border border-gray-100 mx-auto inline-block text-center text-lg font-bold">
-                            Saisons 2005-2015
+                        <Link href={`/niveau-2005-2015`}>
+                            <a className="bg-white hover:bg-gray-100 p-4 rounded-lg shadow border border-gray-100 mx-auto inline-block text-center text-lg font-bold">
+                                Saisons 2005-2015
+                            </a>
                         </Link>
                     </li>
                 </ul>
@@ -33,8 +40,10 @@ export default function Home() {
                 <h2 className="text-3xl font-bold mb-4">Tape noir ou blanc</h2>
                 <ul className="grid grid-cols-1 gap-4">
                     <li className="">
-                        <Link href={`/tape-1`} className="bg-white hover:bg-gray-100 p-4 rounded-lg shadow border border-gray-100 mx-auto inline-block text-center text-lg font-bold">
-                            Tape niveau 1
+                        <Link href={`/tape-1`}>
+                            <a className="bg-white hover:bg-gray-100 p-4 rounded-lg shadow border border-gray-100 mx-auto inline-block text-center text-lg font-bold">
+                                Tape niveau 1
+                            </a>
                         </Link>
                     </li>
                 </ul>
